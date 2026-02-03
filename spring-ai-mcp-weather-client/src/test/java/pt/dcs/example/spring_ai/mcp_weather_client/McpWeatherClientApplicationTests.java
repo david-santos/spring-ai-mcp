b05925caadd616973f3ea79cd35ff571596ea7fb
David Santos <david.c.santos@gmail.com>
2026-02-03 16:05:21 +0000

    Improved documentation

--- spring-ai-mcp-weather-client/src/test/java/pt/dcs/example/spring_ai/mcp_weather_client/McpWeatherClientApplicationTests.java
+++ spring-ai-mcp-weather-client/src/test/java/pt/dcs/example/spring_ai/mcp_weather_client/McpWeatherClientApplicationTests.java
@@ -3,7 +3,7 @@ package pt.dcs.example.spring_ai.mcp_weather_client;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class McpWeatherClientApplicationTests {
 
 	@Test
